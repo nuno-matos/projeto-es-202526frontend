b01c4fda1c00181fdd66c35c60311209a077dab4
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2025-12-25 02:29:19 +0000

    feat: add input fields to the LoginForm

--- src/components/loginLayout/LoginForm.jsx
+++ src/components/loginLayout/LoginForm.jsx
@@ -1,7 +1,9 @@
 import './LoginForm.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
 
 function LoginForm() {
-    return   <div class="container py-5 h-100">
+    return <div class="container py-5 h-100">
                 <div class="row d-flex justify-content-center align-items-center h-100">
                     <div class="col col-xl-10">
                         <div class="card">
@@ -14,7 +16,28 @@ function LoginForm() {
                                         <form>
                                             <div class="d-flex align-items-center mb-3 pb-1"><img src="src\assets\logo.svg" style={{ height: '25px', width: '25px' }} /><span class="h2 fw-bold mb-0 ms-3">Exercise Manager</span></div>
                                             <h5 class="fw-bold mb-3 pb-3" style={{ letterSpacing: '1px' }}>Sign into your account</h5>
-
+                                            <div class="input-wrapper form-outline mb-2">
+                                                <label class="form-label" for="email">Email</label>
+                                                <div class="input-content input-group flex-nowrap">
+                                                    <input class="form-control" type="email" placeholder="johndoe@gmail.com" />
+                                                    <div class="icon input-group-text bg-info text-white">
+                                                        <span>
+                                                            <FontAwesomeIcon icon={faEnvelope} />
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="input-wrapper form-outline mb-2">
+                                                <label class="form-label" for="password">Password</label>
+                                                <div class="input-content input-group flex-nowrap">
+                                                    <input class="form-control" type="password" placeholder="" />
+                                                    <div class="icon input-group-text bg-info text-white">
+                                                        <span>
+                                                            <FontAwesomeIcon icon={faLock} />
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
                                             <div class="pt-1 mb-4 d-grid gap-2"><button type="button" class="btn btn-dark btn-block">Login</button><button type="button" class="btn btn-danger btn-block">Sign Up</button></div>
                                         </form>
                                         <a href="#!" class="small text-muted">Forgot password?</a>
@@ -26,7 +49,7 @@ function LoginForm() {
                         </div>
                     </div>
                 </div>
-            </div>           
-} 
+            </div>
+}
 
 export default LoginForm;
