04dfc345c288b8ed11664e3739202473e3302f39
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2025-12-29 01:21:53 +0000

    feat: add page HomePageLayout

---
+++ src/components/homePageLayout/HomePageLayout.jsx
@@ -0,0 +1,13 @@
+import './HomePageLayout.css'
+
+function HomeLayout() {
+    return (
+    <>
+    <div>
+        <h1 id='titleofpage'>Hello Home</h1>
+    </div>
+    </>
+    )
+}
+
+export default HomeLayout;
