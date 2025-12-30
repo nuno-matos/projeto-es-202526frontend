80ee3c105ae7c2b219acfdf97977a9b30437dbeb
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2025-12-30 02:37:25 +0000

    feat: add routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import LoginForm from "./components/loginLayout/LoginForm"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeLayout from "./components/homePageLayout/HomePageLayout";
+import LoginForm from "./components/loginLayout/LoginForm";
 
-function App() {
-
+function LoginPage() {
   return (
-      <main>
-        <section class="vh-100">
+    <main>
+      <section class="vh-100">
         <div class="container py-5 h-100">
           <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col col-xl-10">
@@ -12,9 +13,18 @@ function App() {
             </div>
           </div>
         </div>
-        </section>
-      </main>
-  )
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomeLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
