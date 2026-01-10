1e11dd4bf3e0d2649799f03223f5d35212e28101
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-01-10 02:07:58 +0000

    feat: add getting roles from the token

--- src/components/homePageLayout/HomePageLayout.jsx
+++ src/components/homePageLayout/HomePageLayout.jsx
@@ -1,11 +1,29 @@
 import './HomePageLayout.css'
 
+function decodeJWT(token) {
+  try {
+    const payload = token.split(".")[1];
+    console.log("PAYLOAD: " + payload)
+    return JSON.parse(atob(payload));
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
 function HomeLayout() {
+    const token = localStorage.getItem("token");
+    const decoded = token ? decodeJWT(token) : null;
+    const role = decoded?.role;
+    console.log("ROLE: " + role)
     return (
     <>
-    <div>
-        <h1 id='titleofpage'>Hello Home</h1>
-    </div>
+        <div>
+            <h1 id='titleofpage'>Hello Home</h1>
+            {role === "TEACHER" && <p>This user is a teacher</p>}
+            {role === "STUDENT" && <p>This user is a student</p>}
+            {!role && <p>User role not available</p>}
+        </div>
     </>
     )
 }
