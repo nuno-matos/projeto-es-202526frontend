b1bb170a3919ff6b230a298407e8c0e52dd02790
Nuno Matos <86886446+nuno-matos@users.noreply.github.com>
2026-02-03 20:28:37 +0000

    refactor: change HomePage to show a user profile

--- src/components/homePageLayout/HomePage.jsx
+++ src/components/homePageLayout/HomePage.jsx
@@ -1,54 +1,103 @@
+import { useState, useEffect } from "react"
 import './HomePage.css'
-import TeacherDashboard from "../dashboard/TeacherDashboard.jsx";
-import StudentDashboard from "../dashboard/StudentDashboard.jsx";
+import { useNavigate } from 'react-router-dom'
 
 function decodeJWT(token) {
-  try {
-    const payload = token.split(".")[1];
-    console.log("PAYLOAD: " + payload)
-    return JSON.parse(atob(payload));
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+    try {
+        const payload = token.split(".")[1];
+        console.log("PAYLOAD: " + payload)
+        return JSON.parse(atob(payload));
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 }
 
-function HomeLayout() {
+function HomePage() {
+
     const token = localStorage.getItem("token");
     const decoded = token ? decodeJWT(token) : null;
     const role = decoded?.role;
-    //const email = decoded?.email
+    const email = decoded?.sub; //subject
 
-    // teste das dashboards -> "TEACHER" ou "STUDENT"
-    // const role = "TEACHER";
-    // const role = "STUDENT";
+    const baseURL = "http://localhost:8080/";
+
+    const [name, setName] = useState("");
+    const [courseUnits, setCourseUnits] = useState([]);
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!token) return;
+
+        fetch(baseURL + "api/user-info", {
+            headers: {
+                   Authorization: 'Bearer ' + token,
+            }
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Failed to fetch user info");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setName(data.name);
+                setCourseUnits(data.courseUnits);
+            })
+            .catch(err => console.error(err));
+    }, [token]);
 
     console.log("ROLE: " + role)
-    //console.log("EMAIL: " + email)
+    console.log("EMAIL: " + email)
+    console.log("NAME: " + name)
+
+    const handleCreateExercise = (courseUnitName) => {
+       navigate('/' + encodeURIComponent(courseUnitName) + '/exercise');
+    };
 
     return (
-        <>
-            <div className="container-fluid p-0">
-                <nav className="navbar navbar-dark bg-dark mb-4 px-4">
-                    <span className="navbar-brand mb-0 h1">Exercise Manager</span>
-                    <button className="btn btn-outline-light btn-sm" onClick={() => {
-                        localStorage.removeItem("token");
-                        window.location.href = "/";
-                    }}>Logout</button>
-                </nav>
+        <div className="container">
+            <div className="row d-flex justify-content-center h-100 mt-5">
+                  <div className="col-12 col-md-8 col-lg-6">
+                    <div className="card card-profile">
+                        <div className="card-header">
+                            {name}
+                        </div>
+                        <div className="card-body">
+                            <div className="col">
+                                <img src="src/assets/user.png" className="user-avatar-icon img-thumbnail rounded float-end" />
+                            </div>
+                            <h5 className="card-title">
+                                {role === "ROLE_TEACHER" && <p>Teacher</p>}
+                                {role === "ROLE_STUDENT" && <p>Student</p>}
+                                {!role && <p>User role not available</p>}
+                            </h5>
+                            <p className="card-text">{"Email: " + email} </p>
+                        </div>
+                        <div className="card-footer text-body-secondary">
+                            Unidades Curriculares
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            {courseUnits.map((courseUnit, index) => (
+                                <li className="list-group-item" key={index}>
+                                    <span>{courseUnit}</span>
 
-                {role === "TEACHER" && <TeacherDashboard />}
-                {role === "STUDENT" && <StudentDashboard />}
-
-                {!role && (
-                    <div className="container text-center mt-5">
-                        <h3>Bem-vindo!</h3>
-                        <p>Por favor faz login para acederes ao painel.</p>
+                                    {role === "ROLE_TEACHER" && (
+                                        <button
+                                            className="btn btn-sm btn-primary float-end"
+                                            onClick={() => handleCreateExercise(courseUnit)}
+                                        >
+                                            Create Exercise
+                                        </button>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                )}
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
-export default HomeLayout;
+export default HomePage
